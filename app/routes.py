ce9322f5d2951f8a145d40366ddb32aa537624c5
Yahya Dalal <yahyadalal05@gmail.com>
2025-12-05 17:36:45 +0000

    step04: added more templates + hard-coded nav links

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,15 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template("index.html")
+
+@main.route('/about')
+def about():
+    return render_template("about.html")
+
+@main.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@main.route('/tasks')
+def tasks():
+    return render_template("tasks.html")
